e02bfcf38ecf13903107102c1965bb94fd3cf932
onowoey <cg10046@naver.com>
2025-08-13 18:51:09 +0900

    Fix: store 모델 분리

--- main/models.py
+++ main/models.py
@@ -7,10 +7,6 @@ class Profile(models.Model):
     profileName = models.CharField(max_length=50)
     profilePhoneNumber = models.CharField(max_length=15)
     
-class Menu(models.Model):
-    menu_name = models.CharField(max_length=50)
-    menu_price = models.IntegerField()
-    
 class Visitor(models.Model):
     gender = models.CharField(max_length=5)
     age = models.IntegerField(default=2)
@@ -19,20 +15,47 @@ class Visitor(models.Model):
     # 2 = 중년
     # 3 = 노년
     is_foreign = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return f'{self.get_gender_display()} / {self.get_age_display()} / 외국인:{self.is_foreign}'
 
+    
+class Type(models.Model):
+    name = models.CharField(max_length=50)
+
+class Category(models.Model):
+    type = models.ForeignKey(Type, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    
+
+    def __str__(self):
+        return f'{self.type.name} / {self.name}'
+
+class Menu(models.Model):
+    menu_name = models.CharField(max_length=50)
+    menu_price = models.IntegerField()
+    
+
+    def __str__(self):
+        return f'{self.store.name} - {self.name}'
+    
 class Store(models.Model):
     userId = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='stores')
     name = models.CharField(max_length=50)
     address = models.TextField()
     latitude = models.FloatField(blank=True, null=True)
     longitude = models.FloatField(blank=True, null=True)
-    kakaoStore = models.IntegerField(blank=True)
-    type = models.TextField(blank=True)
-    category = models.TextField(blank=True)
+    
+    kakaoPlaceId = models.BigIntegerField(blank=True, null=True, unique=True)
+    
+    type = models.ForeignKey(Type, on_delete=models.PROTECT, related_name='stores', blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='stores', blank=True, null=True)
+    
     isWillingCollaborate = models.BooleanField(default=False)
     storeContent = models.TextField(blank=True)
     menu = models.ForeignKey(Menu, on_delete=models.CASCADE, related_name='stores')
     visitor = models.ForeignKey(Visitor, on_delete=models.CASCADE, related_name='stores', null=True, blank=True)
+    
     
 class NewsLetter(models.Model):
     newsId = models.AutoField(primary_key=True)
