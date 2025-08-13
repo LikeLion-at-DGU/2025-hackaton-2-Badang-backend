c2e9698f7445dbb9e3d62cdff23d649da04a169f
onowoey <cg10046@naver.com>
2025-08-13 16:02:56 +0900

    feat: collaborate 모델 설계

--- collaboration/models.py
+++ collaboration/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from main.models import *
 # Create your models here.
+
+class Collaborate(models.Model):
+    requestStore = models.ForeignKey(Store, on_delete=models.CASCADE, related_name='requestCollaborate')
+    responseStore = models.ForeignKey(Store, on_delete=models.CASCADE, related_name='responseCollaborate')
+    isAccepted = models.IntegerField(default=0)
+    requestCreatedAt = models.DateTimeField(auto_now_add=True)
+    createdAt = models.DateTimeField(blank=True)
