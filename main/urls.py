d185a414d8bf1ab42bfa5e561aa84df350e3ad55
onowoey <cg10046@naver.com>
2025-08-18 16:27:15 +0900

    fix: urls.py 경로 수정

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,24 @@
 from rest_framework.routers import DefaultRouter
+from rest_framework import routers
+from django.urls import path, include
+from .views import *
 
 # router = DefaultRouter()
 # router.register()
 
 # urlpatterns = router.urls
 
+app_name = 'main'
+
+user_router = routers.SimpleRouter(trailing_slash=False)
+user_router.register("users", ProfileViewSet, basename="users")
+
+store_router = routers.SimpleRouter(trailing_slash=False)
+store_router.register("stores", StoreViewSet, basename="stores")
+
+
+
 urlpatterns = [
-    
+    path("", include(user_router.urls)),
+    path("", include(store_router.urls)),
 ]
