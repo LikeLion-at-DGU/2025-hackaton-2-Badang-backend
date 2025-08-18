aeffdc3da8c6c333641f5a86d02c2dec4478af9a
onowoey <cg10046@naver.com>
2025-08-18 20:04:44 +0900

    fix: collaborate 모델 수정

--- collaboration/serializers.py
+++ collaboration/serializers.py
@@ -1,16 +1,16 @@
-# from rest_framework import serializers
-# from django.db import transaction
-# from .models import *
-# from main.models import *
+from rest_framework import serializers
+from django.db import transaction
+from .models import *
+from main.models import *
 
-# # 협업 요청한/요청받은 가게 모음 리스트 response DTO
-# class CollaborationListSerializer(serializers.ModelSerializer):
+# 협업 요청한/요청받은 가게 모음 리스트 response DTO
+class CollaborationListSerializer(serializers.ModelSerializer):
     
-#     storeId = serializers.PrimaryKeyRelatedField(source='user', queryset=Profile.objects.all())
+    storeId = serializers.PrimaryKeyRelatedField(source='user', queryset=Profile.objects.all())
     
     
-#     class Meta:
-#         model = Collaborate
+    class Meta:
+        model = Collaborate
         
-# #
+#
 
