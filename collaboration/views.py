59dc55032e941d0c3d7ac0bdf66efd401df826a9
onowoey <cg10046@naver.com>
2025-08-19 05:03:16 +0900

    feat: collaboration serializer 세팅

--- collaboration/views.py
+++ collaboration/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets, status, mixins
+from rest_framework.decorators import action, api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
 # Create your views here.
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+class 
