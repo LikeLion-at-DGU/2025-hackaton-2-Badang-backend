ce9bf12bc8fa03c809b2a74d6ec4295f227c3d7c
onowoey <cg10046@naver.com>
2025-08-20 06:21:00 +0900

    fix: keyword 모델 수정

---
+++ trend/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from django.db import transaction
+from .models import *
+
+class KeywordsInputReq(serializers.Serializer):
+    
+
+
+class KeywordsInputRes(serializers.Serializer):
+    
+    
+    
+class TrendAnalysisByAiReq(serializers.Serializer):
+    
+    
